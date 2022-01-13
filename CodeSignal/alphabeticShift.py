3ec4ce077e30910dfec03b838631a6fd7f08b10f
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2022-01-12 22:57:50 -0500

    Delete CodeSignal directory

--- CodeSignal/alphabeticShift.py
+++
@@ -1,6 +0,0 @@
-def alphabeticShift(inputString):
-    for i in range(len(inputString)):
-        inputString[i] = chr(ord(inputString[i]) + 1)
-    return inputString
-print(chr(98))
-#print(alphabeticShift("abcde"))
--- CodeSignal/depositProfit.py
+++
@@ -1,8 +0,0 @@
-def depositProfit(deposit, rate, threshold):
-    n = 0
-    res = deposit * ((1 + rate/100)**n)
-    while (res < threshold):
-        n += 1
-    return n
-print(2*3)
-print(depositProfit(100 , 20 , 170))
