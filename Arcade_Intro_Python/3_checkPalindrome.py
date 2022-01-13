ad52b81d64930479ff1963074f29e0dd34cbf4dc
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2022-01-12 23:30:57 -0500

    Update 3_checkPalindrome.py

--- Arcade_Intro_Python/3_checkPalindrome.py
+++ Arcade_Intro_Python/3_checkPalindrome.py
@@ -1,5 +1,10 @@
+# Approach 1
 def solution(inputString):
     for i in range(len(inputString) // 2):
         if inputString[i] != inputString[len(inputString) - 1 - i]:
             return False
     return True
+
+# Approach 2
+def solution(inputString):
+    return inputString == inputString[::-1]
